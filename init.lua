84ec582465f563d0fc349171b2c30f0c99a5ba5c
RademerGP <RademerGP@protonmail.com>
2026-01-02 17:42:50 +0000

    Add files via upload

--- init.lua
+++ init.lua
@@ -1,42 +1,36 @@
 -- Matter Capture Tool
 
+local modname = minetest.get_current_modname()
+matter_capture_tool = matter_capture_tool or {}
 
 local USES = 331
 local WEAR_PER_USE = math.floor(65535 / USES)
+
+matter_capture_tool.blacklist = {}
 
 minetest.register_tool("matter_capture_tool:tool", {
     description = "Matter Capture Tool",
     inventory_image = "f_cannon.png",
     wield_image = "f_cannon_wield.png",
     stack_max = 1,
+    liquids_pointable = true,
 
     on_use = function(itemstack, user, pointed_thing)
-        if not user then return itemstack end
+        if not user or pointed_thing.type ~= "node" then
+            return itemstack
+        end
 
         local player_name = user:get_player_name()
         local creative = player_name and minetest.is_creative_enabled(player_name)
+
         local pos = nil
 
-        if pointed_thing.type == "node" then
-            local under = pointed_thing.under
-            local above = pointed_thing.above
+        local under = pointed_thing.under
+        local under_node = minetest.get_node(under)
+        local under_def = minetest.registered_nodes[under_node.name]
 
-            local under_node = minetest.get_node(under)
-            local under_def = minetest.registered_nodes[under_node.name]
-
-            if under_def then
-                pos = under
-            end
-
-            if under_def and under_def.liquidtype == "source" then
-                pos = under
-            elseif above then
-                local above_node = minetest.get_node(above)
-                local above_def = minetest.registered_nodes[above_node.name]
-                if above_def and above_def.liquidtype == "source" then
-                    pos = above
-                end
-            end
+        if under_def then
+            pos = under
         end
 
         if not pos then
@@ -59,17 +53,25 @@ minetest.register_tool("matter_capture_tool:tool", {
             return itemstack
         end
 
+        if node.name and matter_capture_tool.blacklist[node.name] then
+            minetest.chat_send_player(
+                player_name,
+                "Capturing nodes of '" .. node.name ..
+                "' is not allowed (check blacklist in init.lua)."
+            )
+            return itemstack
+        end
+
+        -- containers protection
         local meta = minetest.get_meta(pos)
         local node_inv = meta:get_inventory()
         if node_inv then
             for listname, _ in pairs(node_inv:get_lists()) do
                 if not node_inv:is_empty(listname) then
-                    if player_name then
-                        minetest.chat_send_player(
-                            player_name,
-                            "Cannot capture containers with stored items."
-                        )
-                    end
+                    minetest.chat_send_player(
+                        player_name,
+                        "Cannot capture containers with stored items."
+                    )
                     return itemstack
                 end
             end
@@ -79,12 +81,10 @@ minetest.register_tool("matter_capture_tool:tool", {
         local stack = ItemStack(node.name)
 
         if not player_inv:room_for_item("main", stack) then
-            if player_name then
-                minetest.chat_send_player(
-                    player_name,
-                    "Inventory full. Cannot store captured thing."
-                )
-            end
+            minetest.chat_send_player(
+                player_name,
+                "Inventory full. Cannot store captured node."
+            )
             return itemstack
         end
 
@@ -93,16 +93,13 @@ minetest.register_tool("matter_capture_tool:tool", {
 
         if not creative then
             local wear = itemstack:get_wear()
-            local will_break = (wear + WEAR_PER_USE) >= 65535
-
-            if will_break then
+            if wear + WEAR_PER_USE >= 65535 then
                 minetest.sound_play("default_tool_breaks", {
                     pos = user:get_pos(),
                     gain = 0.7,
                 })
-        end
-
-        itemstack:add_wear(WEAR_PER_USE)
+            end
+            itemstack:add_wear(WEAR_PER_USE)
         end
 
         minetest.sound_play("default_place_node", {
@@ -123,3 +120,11 @@ minetest.register_craft({
         {"default:steel_ingot", "default:diamond", "default:mese_crystal_fragment"},
     }
 })
+
+-- blacklist
+matter_capture_tool.blacklist["default:water_flowing"] = true
+matter_capture_tool.blacklist["default:river_water_flowing"] = true
+matter_capture_tool.blacklist["default:lava_flowing"] = true
+matter_capture_tool.blacklist["protector:protect"] = true
+matter_capture_tool.blacklist["protector:protect2"] = true
+-- matter_capture_tool.blacklist["modname:nodename"] = true
